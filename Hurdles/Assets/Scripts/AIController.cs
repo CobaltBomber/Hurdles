b342e01fc59ca474ffe145fc1d310b9714f234c1
Zach Wadhams <33103466+CobaltBomber@users.noreply.github.com>
2020-10-06 14:45:04 -0600

    Uploaded files
    will update later with more as I progress

---
+++ Hurdles/Assets/Scripts/AIController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : MonoBehaviour
+{
+
+    public bool raceStarted;
+    public float tickTime;
+    public float speed;
+
+    private Rigidbody ai;
+    private Animator animator;
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        animator = GameObject.Find("AIRunner").GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        raceStarted = false;
+        ai = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            raceStarted = true;
+        }
+
+        if (raceStarted == true)
+        {
+            tickTime += Time.deltaTime;
+
+            if (tickTime >= 3.0)
+            {
+                animator.SetTrigger("startTrigger");
+                ai.velocity = new Vector3(speed, 0, 0);
+            }
+        }
+
+    }
+}
---
+++ Hurdles/Assets/Scripts/TimeKeeper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeKeeper : MonoBehaviour
+{
+    public float time;
+    public float tickTime;
+    public Text timeText;
+    public GameObject startText;
+    public GameObject countDown;
+
+    private Animator animator;
+    private GameController hasRaceStarted;
+    private bool raceStarted;
+
+// Start is called before the first frame update
+
+    void Awake()
+    {
+        animator = GameObject.Find("Countdown").GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        raceStarted = false;
+        countDown.SetActive(false);
+        
+     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            raceStarted = true;
+            Destroy(startText);
+            countDown.SetActive(true);
+        }
+        if (raceStarted == true)
+        {
+            animator.SetTrigger("countdownTrigger");
+
+            tickTime += Time.deltaTime;
+            
+            if (tickTime >= 3.0)
+            {
+                time += Time.deltaTime;
+                int x = (int)(time * 100);
+                float y = (float)x / 100.0f;
+                timeText.text = y.ToString();
+            }
+            
+        }
+        
+    }
+}
