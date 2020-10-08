14f5c4e47fa2b20955628c5b6dafafbd52e65f73
Zach Wadhams <33103466+CobaltBomber@users.noreply.github.com>
2020-10-07 20:01:26 -0600

    Added models and animations for player

--- Hurdles/Assets/Scripts/AIController.cs
+++ Hurdles/Assets/Scripts/AIController.cs
@@ -8,7 +8,10 @@ public class AIController : MonoBehaviour
     public bool raceStarted;
     public float tickTime;
     public float speed;
+    public GameObject AITime;
+    public GameObject line;
 
+    private Rigidbody finishLine;
     private Rigidbody ai;
     private Animator animator;
     // Start is called before the first frame update
@@ -16,12 +19,15 @@ public class AIController : MonoBehaviour
     void Awake()
     {
         animator = GameObject.Find("AIRunner").GetComponent<Animator>();
+        AITime.SetActive(false);
     }
 
     void Start()
     {
         raceStarted = false;
         ai = GetComponent<Rigidbody>();
+        finishLine = line.GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
@@ -38,10 +44,47 @@ public class AIController : MonoBehaviour
 
             if (tickTime >= 3.0)
             {
-                animator.SetTrigger("startTrigger");
-                ai.velocity = new Vector3(speed, 0, 0);
+                if (tickTime < 23.0)
+                {
+                    animator.SetTrigger("startTrigger");
+                    ai.velocity = new Vector3(speed, 0, 0);
+                }
+
+                if (tickTime > 23.0)
+                {
+                    animator.SetTrigger("idleTrigger");
+                    ai.velocity = new Vector3(0, 0, 0);
+                    AITime.SetActive(true);
+                }
+                
             }
         }
 
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Jump"))
+        {
+            animator.SetTrigger("jumpTrigger");
+           
+        }
+
+        if (other.gameObject.CompareTag("Stop"))
+        {
+            animator.SetTrigger("idleTrigger");
+        }
+
+        if (other.gameObject.CompareTag("Finishline"))
+        {
+            finishLine.useGravity = true;
+            finishLine.AddForce(transform.forward * 100);
+        }
+
+
+    }
+
+  
+
+
 }
